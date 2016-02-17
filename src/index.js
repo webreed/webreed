00f5351fb1d999f25d4ab48fb759675438c2b0d3
Lea Hayes <lea@rotorz.com>
2016-02-17 11:19:08 +0000

    Add jsdoc comment to main module.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // Copyright (c) Rotorz Limited. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root.
 
+/** @module webreed */
+
+
 export default function setup() {
 }
