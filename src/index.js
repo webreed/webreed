3d6356d3c591dcf3ec867a6c433317ffb618a1f0
Lea Hayes <lea@rotorz.com>
2016-03-04 03:09:39 +0000

    Setup function returns a webreed environment with default mode plugins.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,32 @@
 /** @module webreed */
 
 
+// Webreed Packages
+import Environment from "webreed-core/lib/Environment";
+import ResourceType from "webreed-core/lib/ResourceType";
+
+// Webreed Plugins
+import setupBinaryMode from "webreed-binary-mode";
+import setupTextMode from "webreed-text-mode";
+
+
 export default function setup() {
+  let env = new Environment();
+
+  setupFallbackResourceType(env);
+  setupDefaultPlugins(env);
+
+  return env;
+}
+
+
+function setupFallbackResourceType(env) {
+  let fallbackResourceType = new ResourceType();
+  fallbackResourceType.mode = "binary";
+  env.resourceTypes.set("*", fallbackResourceType);
+}
+
+function setupDefaultPlugins(env) {
+  setupBinaryMode(env, { });
+  setupTextMode(env, { });
 }
--- test/setup.js
+++ test/setup.js
@@ -6,20 +6,61 @@
 import given from "mocha-testdata";
 import should from "should";
 
+// Webreed Core
+import Environment from "webreed-core/lib/Environment";
+import ResourceType from "webreed-core/lib/ResourceType";
+
+// Webreed Plugins
+import BinaryMode from "webreed-binary-mode/lib/BinaryMode";
+import TextMode from "webreed-text-mode/lib/TextMode";
+
 // Project
-import webreed from "../src";
+import setup from "../src";
 
 
 describe("#setup(options)", function () {
 
   it("is a function", function () {
-    webreed
+    setup
       .should.be.a.Function();
   });
 
   it("is named 'setup'", function () {
-    webreed.name
+    setup.name
       .should.be.eql("setup");
+  });
+
+
+  it("returns a webreed environment", function () {
+    let env = setup();
+    env
+      .should.be.instanceOf(Environment);
+  });
+
+
+  it("includes the fallback resource type '*'", function () {
+    let env = setup();
+    env.resourceTypes.get("*")
+      .should.be.instanceOf(ResourceType);
+  });
+
+  it("fallback resource type '*' assumes 'binary' mode", function () {
+    let env = setup();
+    env.resourceTypes.get("*").mode
+      .should.be.eql("binary");
+  });
+
+
+  it("includes the 'binary' mode by default", function () {
+    let env = setup();
+    env.modes.get("binary")
+      .should.be.instanceOf(BinaryMode);
+  });
+
+  it("includes the 'text' mode by default", function () {
+    let env = setup();
+    env.modes.get("text")
+      .should.be.instanceOf(TextMode);
   });
 
 });
